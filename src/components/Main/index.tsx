aea448795e3ca2d462bb991faded64874c5a8737
Pedroabreu155 <pedroliveirabreu@gmail.com>
2022-01-31 12:48:11 -0300

    add first page styled and testr

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,18 @@
+import * as Styled from './styles';
+
 export const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <Styled.Container>
+    <Styled.Logo
+      src="/images/logo.svg"
+      alt="Imagem de um átomo (logo do React) e título React avançado ao lado da imagem."
+    />
+    <Styled.Title>React Avançado</Styled.Title>
+    <Styled.Description>
+      Typescript, NextJS e Styled Components
+    </Styled.Description>
+    <Styled.Illustration
+      src="/images/hero-illustration.svg"
+      alt="Ilustração de um programador"
+    />
+  </Styled.Container>
 );
